02e973ac387cf8b3b1d4d53f5cf6f829e459516f
ant <akongx@gmail.com>
2016-07-20 16:56:13 -0700

    add some comments

--- app.js
+++ app.js
@@ -13,22 +13,29 @@ var url = 'mongodb://localhost:27017/runnerapi';
 var RUNNERS_COLLECTION = "runners";
 var db;
 
+// connect to the database before starting app server
 MongoClient.connect(url, function(err, database) {
   if(err) {
     console.log(err);
     process.exit(1);
   }
+
+  // save database object from callback for reuse
   db = database;
   console.log('Database connection ready');
 
+  // initialize app
   var server = app.listen(3000, function() {
     var port = server.address().port;
     console.log('Example app listening on port', port);
   });
 });
 
+// basic test for the server
 app.get('/', function(req, res) {
   res.send('Hello World');
 });
+
+// define CRUD (create, read, update, delete) routes
 
 
